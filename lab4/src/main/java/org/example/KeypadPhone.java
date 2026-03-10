236630c964598e436c46f00d55247332d5fe0414
kentawy <semchenkokryt@gmail.com>
2026-03-10 15:08:53 +0100

    Add derived classes SmartPhone and KeypadPhone for base model Phone

--- lab4/src/main/java/org/example/KeypadPhone.java
+++ lab4/src/main/java/org/example/KeypadPhone.java
@@ -1,4 +1,29 @@
 package org.example;
 
-public class KeypadPhone {
+/**
+ * Похідний клас для кнопкових телефонів.
+ */
+public class KeypadPhone extends Phone {
+    private boolean hasFlashlight;
+
+    public KeypadPhone(String brand, String model, double price, int storageCapacity, OperatingSystem os, boolean hasFlashlight) {
+        super(brand, model, price, storageCapacity, os);
+        this.hasFlashlight = hasFlashlight;
+    }
+
+    public boolean isHasFlashlight() {
+        return hasFlashlight;
+    }
+
+    public void setHasFlashlight(boolean hasFlashlight) {
+        this.hasFlashlight = hasFlashlight;
+    }
+
+    /**
+     * Перевизначення методу для демонстрації поліморфізму.
+     */
+    @Override
+    public String toString() {
+        return super.toString().replace("}", ", flashlight=" + (hasFlashlight ? "yes" : "no") + "}");
+    }
 }
--- lab4/src/main/java/org/example/SmartPhone.java
+++ lab4/src/main/java/org/example/SmartPhone.java
@@ -1,4 +1,35 @@
 package org.example;
 
-public class SmartPhone {
+/**
+ * Похідний клас для смартфонів.
+ * Демонструє успадкування від базового класу Phone.
+ */
+public class SmartPhone extends Phone {
+    private double cameraMegapixels;
+
+    public SmartPhone(String brand, String model, double price, int storageCapacity, OperatingSystem os, double cameraMegapixels) {
+        // Виклик конструктора базового класу
+        super(brand, model, price, storageCapacity, os);
+        setCameraMegapixels(cameraMegapixels);
+    }
+
+    public double getCameraMegapixels() {
+        return cameraMegapixels;
+    }
+
+    public void setCameraMegapixels(double cameraMegapixels) {
+        if (cameraMegapixels <= 0) {
+            throw new IllegalArgumentException("Камера повинна мати позитивне значення мегапікселів.");
+        }
+        this.cameraMegapixels = cameraMegapixels;
+    }
+
+    /**
+     * Перевизначення методу для демонстрації поліморфізму.
+     */
+    @Override
+    public String toString() {
+        // Беремо рядок базового класу і додаємо специфічну інформацію
+        return super.toString().replace("}", ", camera=" + cameraMegapixels + "MP}");
+    }
 }
