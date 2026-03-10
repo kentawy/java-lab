96121b04fab661d4fd00c37ef11d1987eb0e0e6a
kentawy <semchenkokryt@gmail.com>
2026-03-10 15:22:09 +0100

    Add FoldablePhone class extending SmartPhone

--- lab4/src/main/java/org/example/FoldablePhone.java
+++ lab4/src/main/java/org/example/FoldablePhone.java
@@ -1,4 +1,31 @@
 package org.example;
 
-public class FoldablePhone {
+/**
+ * Клас для складаних смартфонів.
+ * Успадковується від SmartPhone і додає кількість екранів.
+ */
+public class FoldablePhone extends SmartPhone {
+    private int numberOfScreens;
+
+    public FoldablePhone(String brand, String model, double price, int storageCapacity,
+                         OperatingSystem os, double cameraMegapixels, int numberOfScreens) {
+        super(brand, model, price, storageCapacity, os, cameraMegapixels);
+        setNumberOfScreens(numberOfScreens);
+    }
+
+    public int getNumberOfScreens() {
+        return numberOfScreens;
+    }
+
+    public void setNumberOfScreens(int numberOfScreens) {
+        if (numberOfScreens < 2) {
+            throw new IllegalArgumentException("Складаний телефон повинен мати щонайменше 2 екрани.");
+        }
+        this.numberOfScreens = numberOfScreens;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}", ", screens=" + numberOfScreens + "}");
+    }
 }
