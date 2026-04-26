bc74bfa82edf3d68abb7b0534059702186a3ff73
kentawy <semchenkokryt@gmail.com>
2026-04-27 01:39:02 +0200

    Update constructors to support new exceptions.

--- lab4/src/main/java/org/example/SmartPhone.java
+++ lab4/src/main/java/org/example/SmartPhone.java
@@ -1,41 +1,25 @@
 package org.example;
 
-/**
- * Похідний клас для смартфонів.
- * Демонструє успадкування від базового класу Phone.
- */
 public class SmartPhone extends Phone {
     private double cameraMegapixels;
 
-    public SmartPhone(String brand, String model, double price, int storageCapacity, OperatingSystem os, double cameraMegapixels) {
-        // Виклик конструктора базового класу
+    public SmartPhone(String brand, String model, double price, int storageCapacity, OperatingSystem os, double cameraMegapixels) throws InvalidFieldValueException {
         super(brand, model, price, storageCapacity, os);
-        setCameraMegapixels(cameraMegapixels);
-    }
-
-    @Override
-    public String toFileString() {
-        return "SmartPhone;" + getBrand() + ";" + getModel() + ";" + getPrice() + ";" +
-                getStorageCapacity() + ";" + getOs().name() + ";" + cameraMegapixels + ";" + getUuid().toString();
+        this.cameraMegapixels = cameraMegapixels;
     }
 
     public double getCameraMegapixels() {
         return cameraMegapixels;
     }
 
-    public void setCameraMegapixels(double cameraMegapixels) {
-        if (cameraMegapixels <= 0) {
-            throw new IllegalArgumentException("Камера повинна мати позитивне значення мегапікселів.");
-        }
-        this.cameraMegapixels = cameraMegapixels;
+    @Override
+    public String toFileString() {
+        return "SmartPhone;" + getBrand() + ";" + getModel() + ";" + getPrice() + ";" + getStorageCapacity() + ";" + getOs().name() + ";" + cameraMegapixels;
     }
 
-    /**
-     * Перевизначення методу для демонстрації поліморфізму.
-     */
     @Override
     public String toString() {
-        // Беремо рядок базового класу і додаємо специфічну інформацію
-        return super.toString().replace("}", ", camera=" + cameraMegapixels + "MP}");
+        return String.format("SmartPhone{brand='%s', model='%s', price=%.2f, storage=%dGB, OS=%s, camera=%.1fMP}",
+                getBrand(), getModel(), getPrice(), getStorageCapacity(), getOs(), cameraMegapixels);
     }
 }
